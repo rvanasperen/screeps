cec37c5d7a424b13c33d5d15d9ee2d7fc7202b0b
René van Asperen <rvanasperen@gmail.com>
2025-07-18 21:45:54 +0200

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,20 @@
+import screeps from 'rollup-plugin-screeps';
+import typescript from '@rollup/plugin-typescript';
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    input: 'src/main.ts',
+
+    output: {
+        file: 'dist/main.js',
+        format: 'cjs',
+        sourcemap: true,
+    },
+
+    plugins: [
+        typescript(),
+        screeps({
+            configFile: './screeps.json',
+        })
+    ],
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { test } from './test';
+
+export function loop(): void {
+    console.log('loop');
+    test();
+}
